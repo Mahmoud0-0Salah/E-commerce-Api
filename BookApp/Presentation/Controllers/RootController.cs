51903dc1ffc2eb4e91990b9dd48ebe01fa7b0a3f
Mahmoud0-0Salah <mahmoudsalah2339@gmail.com>
2024-09-16 02:00:29 +0300

    Add ApiRoot

---
+++ BookApp/Presentation/Controllers/RootController.cs
@@ -0,0 +1,54 @@
+﻿using Entities.LinkModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        private readonly LinkGenerator _linkGenerator;
+        public RootController(LinkGenerator linkGenerator)=> _linkGenerator = linkGenerator;
+
+        public IActionResult GetRoot()
+        {
+
+            var links = new List<Link>
+            {
+                new Link(_linkGenerator.GetUriByAction(HttpContext,
+                action:"GetRoot",controller:"Root", values: new { }),
+                "self",
+                "GET"),
+
+                new Link(_linkGenerator.GetUriByAction(HttpContext,
+                action:"GetAllCatgoryies",controller:"Catgories", values: new { }),
+                "Get_Catgories",
+                "Get"),
+
+                new Link(_linkGenerator.GetUriByAction(HttpContext,
+                action:"GetAllCatgoryiesOptions",controller:"Catgories", values: new { }),
+                "Options_Catgories",
+                "Options"),
+
+                new Link(_linkGenerator.GetUriByAction(HttpContext,
+                action:"GetAllCatgoryies",controller:"Catgories", values: new { }),
+                "Head_Catgories",
+                "Head"),
+                
+                new Link(_linkGenerator.GetUriByAction(HttpContext,
+                action:"CreateCateogry",controller:"Catgories", values: new { }),
+                "Create_Catgories",
+                "Post")
+            };
+
+            return Ok(links);
+        }
+    }
+}
